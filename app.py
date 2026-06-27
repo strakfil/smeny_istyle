1661db4e6865f64635804dbc246ee4a917fa58b5
strakfil <strakafil@gmail.com>
2026-06-27 22:11:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if 'employee_map' not in st.session_state:
     st.session_state.employee_map = {
         "MAREK STRAKA FT": "MST", "ONDŘEJ TVRDÍK FT": "OTV", "ARPÁD NORCINI FT": "ANO",
         "ELIŠKA DESÁKOVÁ FT": "EDE", "JAN BIŠKO FT": "JB2", "FILIP STRAKA FT": "FIS",
-        "MARIÁN DEBNÁR PT": "MDE", "MICHAL KLUSÁK FT": "MKK","RADEK BOUMA FT": "RBO","SAMUEL ŠVAJKA - 0,75": "SAS"
+        "MARIÁN DEBNÁR PT": "MDE", "MICHAL KLUSÁK FT": "MKK","RADEK BOUMA FT": "RBO","SAMUEL ŠVAJKA - 0,75": "SAS",
         "KRIŠTOF KLUBICA FT": "KKL", "PATRIK KOPULETÝ FT": "PKO", "LUKÁŠ DENER FT": "LDE", "DENISA SUCHÁ FT": "DES",
         "MATĚJ BERAN PT": "MB4", "LUKÁŠ SVOBODA FT": "LSV", "TOMÁŠ FRYDRYCH FT": "TFY", "ŠTĚPÁN JIROUŠEK FT": "JIR"
     }
